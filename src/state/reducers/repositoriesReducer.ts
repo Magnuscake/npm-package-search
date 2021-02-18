a68ba6f635191567091e4a1f667df8de2225b2c9
Magnuscake <saneyar.k@gmail.com>
2021-02-18 22:36:43 +1100

    Finished redux part of the application

--- src/state/reducers/repositoriesReducer.ts
+++ src/state/reducers/repositoriesReducer.ts
@@ -1,33 +1,11 @@
+import { ActionType } from '../actions-types';
+import { Action } from '../actions';
+
 interface RepositoriesState {
   loading: boolean;
   error: string | null;
   data: string[];
 }
-
-enum ActionType {
-  SEARCH_REPOSITORIES = 'SEARCH_REPOSITORIES',
-  SEARCH_REPOSITORIES_SUCCESS = 'SEARCH_REPOSITORIES_SUCCESS',
-  SEARCH_REPOSITORIES_ERROR = 'SEARCH_REPOSITORIES_ERROR',
-}
-
-interface SearchRepositoriesAction {
-  type: ActionType.SEARCH_REPOSITORIES;
-}
-
-interface SearchRepositoriesSuccessAction {
-  type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
-  payload: string[];
-}
-
-interface SearchRepositoriesErrorAction {
-  type: ActionType.SEARCH_REPOSITORIES_ERROR;
-  payload: string;
-}
-
-type Action =
-  | SearchRepositoriesAction
-  | SearchRepositoriesSuccessAction
-  | SearchRepositoriesErrorAction;
 
 const reducer = (
   state: RepositoriesState,
